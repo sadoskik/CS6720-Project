eb2da800759990fa528ec83b6d9d93be5582409f
KB Lee <kangbok.kblee@gmail.com>
2022-12-05 20:25:35 -0500

    Added total metric

--- orchestrator.py
+++ orchestrator.py
@@ -6,6 +6,7 @@ import WFQ
 
 def main():
     iter = 10
+    metric = {'droppedPackets':0, 'receivedBytes':0, 'sentBytes':0, 'latency':0}
     for _ in range(iter):
         total_packets = [[]]*4
         #initialize transmission packets
@@ -15,6 +16,7 @@ def main():
         num_packets = sum([len(x) for x in total_packets])
         wfq = WFQ.WFQ()
         t = 0
+        
         while sum([len(x) for x in total_packets]) != 0 or sum([len(x) for x in wfq.queues]) != 0 or t < wfq.currentPacketFinish:
             for packets in total_packets:
                 if len(packets) == 0:
@@ -24,7 +26,16 @@ def main():
                     wfq.receive(packets.pop(0))
             wfq.process(t)
             t += 1
-        print(wfq.metrics)
+        for key, val in wfq.metrics.items():
+            if key == 'latency':
+                metric[key] += val/num_packets
+            else:
+                metric[key] += val
+    for key, val in metric:
+        metric[key] = val/iter
+    print(metric)
+
+        
         
         
         
